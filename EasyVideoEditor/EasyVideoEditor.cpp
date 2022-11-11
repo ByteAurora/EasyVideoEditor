366ff7034352835552415f2ec003c6a416dbdc8a
Sohn Young Jin <sonkim1001@naver.com>
2022-11-11 13:47:00 +0900

    Change name of centralWidget and menuBar

--- EasyVideoEditor/EasyVideoEditor.cpp
+++ EasyVideoEditor/EasyVideoEditor.cpp
@@ -4,6 +4,7 @@ EasyVideoEditor::EasyVideoEditor(QWidget *parent)
     : QMainWindow(parent)
 {
     ui.setupUi(this);
+    Widgets::getInstance()->initWidgets(this);
 }
 
 EasyVideoEditor::~EasyVideoEditor()
--- EasyVideoEditor/EasyVideoEditor.h
+++ EasyVideoEditor/EasyVideoEditor.h
@@ -2,6 +2,7 @@
 
 #include <QtWidgets/QMainWindow>
 #include "ui_EasyVideoEditor.h"
+#include "Widgets.h"
 
 class EasyVideoEditor : public QMainWindow
 {
