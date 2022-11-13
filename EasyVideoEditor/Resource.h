23913ab6e816f4746c9e1022801d15c6cf6bdb50
Sohn Young Jin <sonkim1001@naver.com>
2022-11-14 02:56:10 +0900

    Add comments for Resource class

--- EasyVideoEditor/Resource.h
+++ EasyVideoEditor/Resource.h
@@ -2,29 +2,57 @@
 
 #include <iostream>
 
+/*
+* @version  v1.0.0
+* @author   Sohn Young Jin
+* @since    v1.0.0
+*
+* The EveProject class is a class of the singleton pattern that is created when the program is executed.
+* All frame objects currently being modified in the program and resource(videos, images) being used are managed in a list.
+*/
 class Resource
 {
 public:
+    // An enumeration of the type of resource.
     enum ResourceType {
         IMAGE, VIDEO
     };
 protected:
+    // The index of that resource in EveProject.videoList or EveProject.imageList.
     int resourceId;
+
+    // Resource type(ResourceType.IMAGE or ResourceType.VIDEO).
     ResourceType resourceType;
+
+    // Resource path.
     std::string resourcePath;
 public:
+    // Default constructor.
     Resource();
+
+    // Default destructor.
     ~Resource();
 
+    // A pure virtual function that must implement the resource loading process.
     virtual bool loadResource() = 0;
+
+    // A pure virtual function that must be implemente the process of returning a resource.
+    // The return value is void*, and it is used after converting to Video* or Image* depending on the type of resource.
     virtual void* getResource() = 0;
 public:
+    // Resource ID setter.
     void setResourceId(int resourceId);
-    void setResourceType(ResourceType resourceType);
-    void setResourcePath(std::string resourcePath);
-
+    // Resource ID getter.
     int getResourceId();
+
+    // Resource type setter.
+    void setResourceType(ResourceType resourceType);
+    // Resource type getter.
     ResourceType getResourceType();
+
+    // Resource path setter.
+    void setResourcePath(std::string resourcePath);
+    // Resource path getter.
     std::string getResourcePath();
 };
 
