94b6f2f048c475ece419ef4cc8b0988051eba317
Sohn Young Jin <sonkim1001@naver.com>
2022-11-14 02:47:51 +0900

    Add comments for Frame class

--- EasyVideoEditor/Frame.h
+++ EasyVideoEditor/Frame.h
@@ -5,30 +5,65 @@
 #include <string>
 #include "Command.h"
 
+/*
+* @version  v1.0.0
+* @author   Sohn Young Jin
+* @since    v1.0.0
+*
+* The Frame class is used for efficient memory management.
+* Instead of data about the actual frame, it has information about the original video to which the frame belongs and
+* the index of the frame in the video. In addition, all commands(tasks) applied to the frame are saved in list format.
+*/
 class Frame
 {
 private:
+    // List of all commands applied to the frame.
     std::list<Command*> commandList;
+
+    // Index of the video to which the frame belongs among the videos in EveProject.videoList.
     int sourceId;
+
+    // The index of the frame in the image containing the frame.
     int sourceFrameIndex;
 public:
+    // Default constructor.
     Frame();
+
+    // Default destructor.
     ~Frame();
 
+    // Set frame informations.
     void setFrameInfo(int sourceId, int sourceFrameIndex);
+    // Get command corresponding to the index.
     Command* getCommand(int index);
 
+    // Add command into command list.
     void addCommand(Command* command);
+
+    // Remove all command in command list.
     void removeAllCommand();
+
+    // Remove last added command in command list.
     void removeLastCommand();
+
+    // Remove command corresponding to the index.
     void removeCommand(int index);
+
+    // Remove command which same as parameter.
     void removeCommand(Command* command);
 public:
+    // Source ID setter.
     void setSourceId(int sourceId);
-    void setSourceFrameIndex(int sourceFrameIndex);
-    void setCommandList(std::list<Command*> commandList);
     int getSourceId();
+
+    // Source frame index setter.
+    void setSourceFrameIndex(int sourceFrameIndex);
+    // Source frame index getter.
     int getSourceFrameIndex();
+
+    // Command list setter.
+    void setCommandList(std::list<Command*> commandList);
+    // Command list getter.
     std::list<Command*>* getCommandList();
 };
 
