1eae4b6335d0b99f26783721d048c22e49ca57b1
Sohn Young Jin <sonkim1001@naver.com>
2022-11-14 13:51:07 +0900

    Add comments and custom constructor

--- EasyVideoEditor/Image.cpp
+++ EasyVideoEditor/Image.cpp
@@ -1,7 +1,26 @@
 #include "Image.h"
 
 Image::Image() {
+    resourceType = IMAGE;
+    resourceId = 0;
+    resourcePath = "";
+}
 
+Image::Image(int resourceId) {
+    this->resourceId = resourceId;
+    this->resourcePath = "";
+}
+
+Image::Image(std::string resourcePath) {
+    resourceType = IMAGE;
+    resourceId = 0;
+    this->resourcePath = resourcePath;
+}
+
+Image::Image(int resourceId, std::string resourcePath) {
+    resourceType = IMAGE;
+    this->resourceId = resourceId;
+    this->resourcePath = resourcePath;
 }
 
 Image::~Image() {
@@ -9,10 +28,10 @@ Image::~Image() {
 }
 
 bool Image::loadResource() {
-    mat = cv::imread(resourcePath, cv::IMREAD_COLOR);
-    return !mat.empty();
+    image = cv::imread(resourcePath, cv::IMREAD_COLOR);
+    return !image.empty();
 }
 
 void* Image::getResource(){
-    return &mat;
+    return &image;
 }
--- EasyVideoEditor/Image.h
+++ EasyVideoEditor/Image.h
@@ -3,15 +3,36 @@
 #include <opencv2/opencv.hpp>
 #include "Resource.h"
 
+/*
+* @version  v1.0.0
+* @author   Sohn Young Jin
+* @since    v1.0.0
+*
+* The Image class is a class that inherits Resource class.
+* Stores information about image resources used in EveProject.
+*/
 class Image : public Resource
 {
 private:
-    cv::Mat mat;
+    // Mat to use image in opencv.
+    cv::Mat image;
 public:
+    // Default constructor.
     Image();
+
+    // Custom constructor.
+    Image(int resourceId);
+    Image(std::string resourcePath);
+    Image(int resourceId, std::string resourcePath);
+
+    // Default destructor.
     ~Image();
 
+    // A pure virtual function that must implement the resource loading process.
     virtual bool loadResource();
+
+    // A pure virtual function that must be implement the process of returning a resource.
+    // The return value is void*, and it is used after converting to Video* or Image* depending on the type of resource.
     virtual void* getResource();
 };
 
