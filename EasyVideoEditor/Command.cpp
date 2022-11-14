491958981dedbfd3627f6eb98240fa9b74123aab
Sohn Young Jin <sonkim1001@naver.com>
2022-11-14 14:34:29 +0900

    Add comments, pure virtual function and enumeration of command type

--- EasyVideoEditor/Command.cpp
+++ EasyVideoEditor/Command.cpp
@@ -1,1 +1,17 @@
 #include "Command.h"
+
+Command::Command() {
+
+}
+
+Command::~Command() {
+
+}
+
+void Command::setCommand(Command::CommandType commandType) {
+    this->commandType = commandType;
+}
+
+Command::CommandType Command::getCommand() {
+    return commandType;
+}
--- EasyVideoEditor/Command.h
+++ EasyVideoEditor/Command.h
@@ -1,5 +1,27 @@
 #pragma once
 class Command
 {
-};
+private:
+    // An enumeration of the type of command.
+    enum CommandType {
+        COLOR_EMPHASIS, CHANGE_BRIGHTNESS, CHANGE_CONSTRAST, FILTER_EFFECT, CHROMAKY, ANIMATION
+        , ADD_IMAGE, ADD_VIDEO, CUT_VIDEO, RESIZE_RESOLUTION, CHANGE_PLAY_SPEED, ADD_SUBTITLE
+    };
 
+    // Command type.
+    CommandType commandType;
+public:
+    // Default constructor.
+    Command();
+
+    // Default destructor.
+    ~Command();
+
+    // A pure virtual function overloaded with an operator that must be implemented in relation to a command in a class that inherits Command class.
+    virtual void operator()() = 0;
+
+    // Command type setter.
+    void setCommand(CommandType commandType);
+    // Command type getter.
+    CommandType getCommand();
+};
