3a43899384b4342b6547b06ebef0ed1ca9ea37c2
Sohn Young Jin <sonkim1001@naver.com>
2022-11-20 15:27:26 +0900

    Add widgets event(Slider, SideMenu)

--- EasyVideoEditor/EasyVideoEditor.cpp
+++ EasyVideoEditor/EasyVideoEditor.cpp
@@ -5,7 +5,47 @@ EasyVideoEditor::EasyVideoEditor(QWidget *parent)
 {
     ui.setupUi(this);
     Widgets::getInstance()->initWidgets(this);
+
+    // Init ui.
+    ui.btn_coloremphasis->setCheckable(true);
+    ui.edt_coloremphasis_red->setValidator(new QIntValidator(0, 255, this));
+    ui.edt_coloremphasis_green->setValidator(new QIntValidator(0, 255, this));
+    ui.edt_coloremphasis_blue->setValidator(new QIntValidator(0, 255, this));
+
+    // Init data.
+    new SideMenu(ui.btn_coloremphasis, ui.w_coloremphasis);
+    new SideMenu(ui.btn_changebrightness, ui.w_changebrightness);
+    SideMenu::selectSideMenu(ui.btn_coloremphasis);
+
+    // Init signal, slot.
+    connect(ui.btn_coloremphasis, SIGNAL(clicked()), this, SLOT(sideMenuClicked()));
+    connect(ui.btn_changebrightness, SIGNAL(clicked()), this, SLOT(sideMenuClicked()));
+    connect(ui.sd_coloremphasis_red, SIGNAL(valueChanged(int)), this, SLOT(setLineEditBySlider(int)));
+    connect(ui.sd_coloremphasis_green, SIGNAL(valueChanged(int)), this, SLOT(setLineEditBySlider(int)));
+    connect(ui.sd_coloremphasis_blue, SIGNAL(valueChanged(int)), this, SLOT(setLineEditBySlider(int)));;
+    connect(ui.edt_coloremphasis_red, SIGNAL(textChanged(QString)), this, SLOT(setSliderByLineEdit(QString)));;
+    connect(ui.edt_coloremphasis_green, SIGNAL(textChanged(QString)), this, SLOT(setSliderByLineEdit(QString)));
+    connect(ui.edt_coloremphasis_blue, SIGNAL(textChanged(QString)), this, SLOT(setSliderByLineEdit(QString)));
 }
 
 EasyVideoEditor::~EasyVideoEditor()
 {}
+
+void EasyVideoEditor::sideMenuClicked() {
+    SideMenu::selectSideMenu((QPushButton*)sender());
+}
+
+void EasyVideoEditor::setLineEditBySlider(int value) {
+    QObject* senderObject = sender();
+    if (senderObject == ui.sd_coloremphasis_red) ui.edt_coloremphasis_red->setText(QString::number(value));
+    else if (senderObject == ui.sd_coloremphasis_green) ui.edt_coloremphasis_green->setText(QString::number(value));
+    else if (senderObject == ui.sd_coloremphasis_blue) ui.edt_coloremphasis_blue->setText(QString::number(value));
+
+}
+
+void EasyVideoEditor::setSliderByLineEdit(QString value) {
+    QObject* senderObject = sender();
+    if (senderObject == ui.edt_coloremphasis_red) ui.sd_coloremphasis_red->setValue(value.toInt());
+    else if (senderObject == ui.edt_coloremphasis_green) ui.sd_coloremphasis_green->setValue(value.toInt());
+    else if (senderObject == ui.edt_coloremphasis_blue) ui.sd_coloremphasis_blue->setValue(value.toInt());
+}
--- EasyVideoEditor/EasyVideoEditor.h
+++ EasyVideoEditor/EasyVideoEditor.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include <QtWidgets/QMainWindow>
+#include <QRegularExpression>
+#include <QValidator>
+#include "SideMenu.h"
 #include "ui_EasyVideoEditor.h"
 #include "Widgets.h"
 
@@ -14,4 +17,9 @@ public:
 
 private:
     Ui::EasyVideoEditorClass ui;
+
+private slots:
+    void setLineEditBySlider(int value);
+    void setSliderByLineEdit(QString value);
+    void sideMenuClicked();
 };
