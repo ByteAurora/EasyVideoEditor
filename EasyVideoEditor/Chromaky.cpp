e13bd831a0efeb73b979545f7a4b708ea8ee6880
Sohn Young Jin <sonkim1001@naver.com>
2022-11-20 10:55:05 +0900

    Implementation Chromaky class

--- EasyVideoEditor/Chromaky.cpp
+++ EasyVideoEditor/Chromaky.cpp
@@ -1,1 +1,43 @@
 #include "Chromaky.h"
+
+Chromaky::Chromaky() {
+    hueMin = 0;
+    hueMax = 180;
+    saturationMin = 0;
+    saturationMax = 255;
+    valueMin = 0;
+    valueMax = 255;
+    reverse = false;
+    backgroundImageResourceId = 0;
+}
+
+Chromaky::Chromaky(int hueMin, int hueMax, int saturationMin, int saturationMax, int valueMin, int valueMax, bool reverse, int backgroundImageResourceId) {
+    this->hueMin = hueMin;
+    this->hueMax = hueMax;
+    this->saturationMin = saturationMin;
+    this->saturationMax = saturationMax;
+    this->valueMin = valueMin;
+    this->valueMax = valueMax;
+    this->reverse = reverse;
+    this->backgroundImageResourceId = backgroundImageResourceId;
+}
+
+Chromaky::~Chromaky() {
+
+}
+
+void Chromaky::operator()() {
+
+}
+
+void Chromaky::operator()(cv::Mat* mat) {
+    cv::Mat hsv;
+    cvtColor(*mat, hsv, cv::COLOR_BGR2HSV);
+    cv::Mat mask;
+    cv::inRange(hsv, cv::Scalar(hueMin, saturationMin, valueMin), cv::Scalar(hueMax, saturationMax, valueMax), mask);
+    if (reverse) mask = ~mask;
+
+    cv::Mat background = ((cv::Mat*)EveProject::getInstance()->getImage(backgroundImageResourceId)->getResource())->clone();
+
+    copyTo(*mat, background, mask);
+}
--- EasyVideoEditor/Chromaky.h
+++ EasyVideoEditor/Chromaky.h
@@ -1,7 +1,22 @@
 #pragma once
 #include "Command.h"
-class Chromaky :
-    public Command
+#include "EveProject.h"
+
+class Chromaky : public Command
 {
+private:
+    int hueMin, hueMax;
+    int saturationMin, saturationMax;
+    int valueMin, valueMax;
+    bool reverse;
+    int backgroundImageResourceId;
+public:
+    Chromaky();
+    Chromaky(int hueMin, int hueMax, int saturationMin, int saturationMax, int valueMin, int valueMax, bool reverse, int backgroundImageResourceId);
+    ~Chromaky();
+
+    virtual void operator()();
+    virtual void operator()(cv::Mat* mat);
 };
+
 
