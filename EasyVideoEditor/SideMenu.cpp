630f6430e2079e0b9fc80a4d5515af8aa2893ece
Sohn Young Jin <sonkim1001@naver.com>
2022-11-20 15:25:02 +0900

    Implement SideMenu class

--- EasyVideoEditor/SideMenu.cpp
+++ EasyVideoEditor/SideMenu.cpp
@@ -1,1 +1,46 @@
 #include "SideMenu.h"
+
+std::vector<SideMenu*> SideMenu::sideMenuList;
+
+SideMenu::SideMenu() {
+    sideMenuButton = NULL;
+    sideMenuPage = NULL;
+    sideMenuList.push_back(this);
+}
+
+SideMenu::SideMenu(QPushButton* sideMenuButton, QWidget* sideMenuPage) {
+    this->sideMenuButton = sideMenuButton;
+    this->sideMenuPage = sideMenuPage;
+    this->sideMenuButton->setCheckable(true);
+    sideMenuList.push_back(this);
+}
+
+SideMenu::~SideMenu() {
+
+}
+
+void SideMenu::selectSideMenu(QPushButton* sideMenuButton) {
+    for (int loop = 0; loop < sideMenuList.size(); loop++)
+        sideMenuList[loop]->select(sideMenuList[loop]->sideMenuButton == sideMenuButton);
+}
+
+void SideMenu::select(bool selectState) {
+    sideMenuButton->setChecked(selectState);
+    sideMenuPage->setVisible(selectState);
+}
+
+void SideMenu::setSideMenuButton(QPushButton* sideMenuButton) {
+    this->sideMenuButton = sideMenuButton;
+}
+
+QPushButton* SideMenu::getSideMenuButton() {
+    return sideMenuButton;
+}
+
+void SideMenu::setSideMenuPage(QWidget* sideMenuPage) {
+    this->sideMenuPage = sideMenuPage;
+}
+
+QWidget* SideMenu::getSideMenuPage() {
+    return sideMenuPage;
+}
--- EasyVideoEditor/SideMenu.h
+++ EasyVideoEditor/SideMenu.h
@@ -1,5 +1,27 @@
 #pragma once
+
+#include <QPushButton>
+#include <QWidget>
+#include <iostream>
+#include <vector>
+
 class SideMenu
 {
+private:
+    static std::vector<SideMenu*> sideMenuList;
+    QPushButton* sideMenuButton;
+    QWidget* sideMenuPage;
+public:
+    SideMenu();
+    SideMenu(QPushButton* sideMenuButton, QWidget* sideMenuPage);
+    ~SideMenu();
+
+    static void selectSideMenu(QPushButton* sideMenuButton);
+    void select(bool selectState);
+public:
+    void setSideMenuButton(QPushButton* sideMenuButton);
+    QPushButton* getSideMenuButton();
+    void setSideMenuPage(QWidget* sideMenuPage);
+    QWidget* getSideMenuPage();
 };
 
