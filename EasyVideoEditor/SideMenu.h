f3eb01d17a91ed1bbc21b3635a36083baa166c94
Sohn Young Jin <sonkim1001@naver.com>
2022-11-21 12:42:32 +0900

    Add comments

--- EasyVideoEditor/SideMenu.h
+++ EasyVideoEditor/SideMenu.h
@@ -5,23 +5,48 @@
 #include <iostream>
 #include <vector>
 
+/*
+* @version  v1.0.0
+* @author   Sohn Young Jin
+* @since    v1.0.0
+*
+* The SideMenu class is a class connected to SideMenu-related widgets in EveProject.
+*/
 class SideMenu
 {
 private:
+    // A list containing all SideMenu created.
     static std::vector<SideMenu*> sideMenuList;
+
+    // Button on side menu.
     QPushButton* sideMenuButton;
+
+    // Page on side menu.
     QWidget* sideMenuPage;
 public:
+    // Default constructor.
     SideMenu();
+
+    // Custom constructor.
     SideMenu(QPushButton* sideMenuButton, QWidget* sideMenuPage);
+
+    // Default destructor.
     ~SideMenu();
 
+    // Select side menu and change all side menus widgets state.
     static void selectSideMenu(QPushButton* sideMenuButton);
+
+    // Change side menu widgets state.
     void select(bool selectState);
 public:
+    // Side menu button setter.
     void setSideMenuButton(QPushButton* sideMenuButton);
+    // Side menu button getter.
     QPushButton* getSideMenuButton();
+
+    // Side menu page setter.
     void setSideMenuPage(QWidget* sideMenuPage);
+    // Side menu page getter.
     QWidget* getSideMenuPage();
 };
 
